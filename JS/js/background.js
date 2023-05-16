b676f74305078c481b862c13d4045685e0429383
Sejong Yoo <ysj7705@gmail.com>
2023-05-16 22:14:10 +0900

    5/16

--- JS/js/background.js
+++ JS/js/background.js
@@ -1,17 +1,19 @@
 // const images = ['1.jpeg', '2.jpeg', '3.jpeg', '4.jpeg', '5.jpeg', '6.png'];
-const images = ['6.png'];
-const h2 = document.querySelector('#clock');
-
+const body = document.querySelector("body");
+const images = ["6.png", "1.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+body.style.backgroundImage = `url(./img/${chosenImage}) `;
+const h2 = document.querySelector("#clock");
 
 // console.log(chosenImage)
 
-const bgImage = document.createElement('img'); // 자바스립트에서 html에 추가할 element 생성
+// const bgImage = document.createElement("img"); // 자바스립트에서 html에 추가할 element 생성
 
-bgImage.src = `img/${chosenImage}`;
+// bgImage.src = `img/${chosenImage}`;
 
 // console.log(bgImage);
 
 // document.body.appendChild(bgImage)  // body 마지막에 노드 추가
 
-document.body.insertBefore(bgImage, h2); // 해당 h2 노드 앞에 추가
+// document.body.insertBefore(bgImage, h2); // 해당 h2 노드 앞에 추가
