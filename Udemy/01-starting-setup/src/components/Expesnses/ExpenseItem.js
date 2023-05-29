6145564d322e4f2190c0cead419586f768a163af
Sejong Yoo <ysj7705@gmail.com>
2023-05-29 17:18:44 +0900

    5/28

--- Udemy/01-starting-setup/src/components/Expesnses/ExpenseItem.js
+++ Udemy/01-starting-setup/src/components/Expesnses/ExpenseItem.js
@@ -1,15 +1,20 @@
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked!!!");
+  };
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
-    </div>
+    </Card>
   );
 }
 export default ExpenseItem;
