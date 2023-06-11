f4270d7a6cd8c45e6a1c1470c20853dc6b05e21e
Sejong Yoo <ysj7705@gmail.com>
2023-06-11 17:51:28 +0900

    6/11 비용 추적기 final

--- Udemy/01-starting-setup/src/components/Expesnses/Expenses.js
+++ Udemy/01-starting-setup/src/components/Expesnses/Expenses.js
@@ -3,30 +3,29 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseList from "./ExpenseList";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 function Expenses(props) {
-  const [filteredYear, setfilteredYear] = useState("2020");
+	const [filteredYear, setfilteredYear] = useState("2020");
 
-  const filterChangeHandler = (selectedYear) => {
-    setfilteredYear(selectedYear);
-  };
+	const filterChangeHandler = (selectedYear) => {
+		setfilteredYear(selectedYear);
+	};
 
-  const filteredExpenses = props.items.filter((expense) => {
-    return expense.date.getFullYear().toString() === filteredYear;
-  });
+	const filteredExpenses = props.items.filter((expense) => {
+		return expense.date.getFullYear().toString() === filteredYear;
+	});
 
-  return (
-    <li>
-      <Card className="expenses">
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-        <ExpenseList items={filteredExpenses} />
-      </Card>
-    </li>
-  );
+	return (
+		<li>
+			<Card className="expenses">
+				<ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+				<ExpensesChart expenses={filteredExpenses} />
+				<ExpenseList items={filteredExpenses} />
+			</Card>
+		</li>
+	);
 }
 
 export default Expenses;
